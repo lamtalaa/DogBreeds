084f8ddcd5e75b7888d82aeac4ca56f1f8fc8eb6
Hasan Zaidi <hasan.zaidi@techconsulting.tech>
2025-06-10 10:11:16 -0400

    Showing dog images

--- DogBreeds/DogBreeds/DogBreedsApp.swift
+++ DogBreeds/DogBreeds/DogBreedsApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct DogBreedsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            DogBreedsListView()
         }
     }
 }
