ea2a7d1666a5c753b0edc8cfcead31a527013a3b
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2025-06-10 09:12:29 -0400

    adding files structure

---
+++ DogBreeds/DogBreeds/Models/testModel.swift
@@ -0,0 +1,8 @@
+//
+//  testModel.swift
+//  DogBreeds
+//
+//  Created by Yassine Lamtalaa on 6/10/25.
+//
+
+import Foundation
---
+++ DogBreeds/DogBreeds/Network/testNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  testNetwork.swift
+//  DogBreeds
+//
+//  Created by Yassine Lamtalaa on 6/10/25.
+//
+
+import Foundation
